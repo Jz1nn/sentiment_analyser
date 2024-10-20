f62135b3048acf3f40c059e6a84e3f9057972889
Jz1nn <jz1nnwln@gmail.com>
2024-10-20 13:17:25 -0300

    Post Project

---
+++ sentiment.py
@@ -0,0 +1,40 @@
+# %% [markdown]
+# # Imports
+
+# %%
+import nltk
+import streamlit as st
+
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+# %% [markdown]
+# # Creation of the system
+
+# %%
+# System Title
+
+st.write( '# Customer Satisfaction Analysis' )
+
+user_input = st.text_input( 'Please rate our service:' )
+
+# %% [markdown]
+# # Creation of the predictive customer satisfaction machine
+
+# %%
+nltk.download( 'vader_lexicon' )
+
+s = SentimentIntensityAnalyzer()
+
+score = s.polarity_scores( user_input )
+
+if score == 0:
+    st.write( '### Neutral' )
+
+elif score[ 'neg' ] != 0:
+    st.write( '### Negative' )
+
+elif score[ 'pos' ] != 0:
+    st.write( '### Positive' )
+
+
+
